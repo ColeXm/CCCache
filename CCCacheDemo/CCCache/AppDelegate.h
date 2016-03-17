ffd2952d01b1a024c06225b19b2f74c241cc3c2f
xiamin <xm10149@ly.com>
2016-03-17 15:29:55 +0800

    no message

---
+++ CCCacheDemo/CCCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCCache
+//
+//  Created by ColeXm on 16/3/17.
+//  Copyright © 2016年 ColeXm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCCacheDemo/CCCache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCCache
+//
+//  Created by ColeXm on 16/3/17.
+//  Copyright © 2016年 ColeXm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
